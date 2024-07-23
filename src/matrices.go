9e681ccaff0faae25d1082ecffdc0ee82e2dc8cd
aseem sethi <aseemsethi@yahoo.com>
2024-07-23 06:20:20 +0000

    added I matrix

--- src/matrices.go
+++ src/matrices.go
@@ -7,9 +7,16 @@ import (
 	"fmt"
 )
 
+type Matrix struct {
+	Rows uint
+	Cols uint
+	Data [][]complex128
+}
+
 type VecQbits struct {
-	N    uint
-	Data []complex128
+	N           uint
+	Data        []complex128 // value of Qbit
+	Probability []float64    // Probablity of each vector
 }
 
 // Creates n Vectors each with a value of |0> and returns them in an array
@@ -17,10 +24,60 @@ func NewVector(n uint) VecQbits {
 	var data []complex128
 	var i uint
 
-	fmt.Printf("New Vector...")
-
 	for i = 0; i < n; i++ {
 		data = append(data, complex128(0))
 	}
 	return VecQbits{N: n, Data: data}
+}
+
+func NewMatrix(r, c uint) Matrix {
+	var data = make([][]complex128, r)
+	var i, j uint
+	for i = 0; i < r; i++ {
+		data[i] = make([]complex128, c)
+		for j = 0; j < c; j++ {
+			data[i][j] = complex128(0)
+		}
+	}
+	return Matrix{Rows: r, Cols: c, Data: data}
+}
+
+func NewIMatrix(r, c uint) Matrix {
+	m := NewMatrix(r, c)
+	var i, j uint
+	for i = 0; i < m.Rows; i++ {
+		for j = 0; j < m.Cols; j++ {
+			if i == j {
+				m.Set(i, j, 1)
+			}
+		}
+	}
+	return m
+}
+
+func (m *Matrix) Print() {
+	var i, j uint
+	for i = 0; i < m.Rows; i++ {
+		for j = 0; j < m.Cols; j++ {
+			fmt.Printf("%.1f ", m.At(i, j))
+		}
+		fmt.Println("")
+	}
+}
+
+func (m *Matrix) Set(r, c uint, val complex128) {
+	m.Data[r][c] = val
+}
+
+func (m *Matrix) SetAll(val complex128) {
+	var i, j uint
+	for i = 0; i < m.Rows; i++ {
+		for j = 0; j < m.Cols; j++ {
+			m.Set(i, j, val)
+		}
+	}
+}
+
+func (m *Matrix) At(r, c uint) complex128 {
+	return m.Data[r][c]
 }
--- src/sim.go
+++ src/sim.go
@@ -1,4 +1,5 @@
 // Learnings from https://github.com/takezo5096/goqkit
+// and https://github.com/itsubaki/q/blob/main/q.go
 
 package main
 
@@ -7,28 +8,34 @@ import (
 )
 
 type Qsim struct {
-	numQbits uint
-	vec      VecQbits
-	regQsim  []*Reg
+	numQbits uint     // number of Qbits
+	vec      VecQbits // Struct - having number of bits and their vector
+	regQsim  []*Reg   // Not sure of this
 	Name     string
 }
 
 func MakeCircuit(num uint, name string) Qsim {
 	//var i uint
 	//i = 1 << num
-	reg := make([]*Reg, 0)
+	reg := make([]*Reg, 0) // TBD - remove
 	// Create a |0> vector for each of the input Qubits
 	// and save in Circuit->vec
+	fmt.Printf("Create 2 Qbits and assign |0>\n")
 	vec := NewVector(num)
+	fmt.Printf("%+v\n\n", vec)
 
-	fmt.Printf("MakeCircuit with %d registers\n", num)
+	fmt.Printf("MakeCircuit with these %d Qbits as inputs\n", num)
 	return Qsim{numQbits: num, vec: vec, regQsim: reg, Name: name}
 
 }
 
 func main() {
-	fmt.Println("Qsim start...")
+	fmt.Println("Qsim starting...........................")
+	// Create a circuit with n inputs, each input is |0>
 	ckt := MakeCircuit(2, "Circuit1")
-	fmt.Printf("Circuit %+v\n", ckt)
+	fmt.Printf("Circuit %+v\n\n", ckt)
+	fmt.Printf("Create Identity Matrix\n")
+	iMatrix := NewIMatrix(2, 2)
+	iMatrix.Print()
 
 }
